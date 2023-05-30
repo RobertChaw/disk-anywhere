d0a26924aabd54363fb4f54cd29005fad2455fc6
RobertChaw <1335952506@qq.com>
2023-05-30 20:13:05 +0800

    chore: support dotenv

--- src/api/configuration.ts
+++ src/api/configuration.ts
@@ -1,10 +1,26 @@
 import { createConfiguration, hooks } from "@midwayjs/hooks";
 import * as Koa from "@midwayjs/koa";
+import * as cos from "@midwayjs/cos";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 /**
  * setup midway server
  */
 export default createConfiguration({
-  imports: [Koa, hooks()],
-  importConfigs: [{ default: { keys: "session_keys" } }],
+  imports: [Koa, hooks(), cos],
+  importConfigs: [
+    {
+      default: {
+        keys: "bf21a14d8b0eb60f8b95ae0df61162b8",
+        cos: {
+          client: {
+            SecretId: process.env.COS_SECRET_ID,
+            SecretKey: process.env.COS_SECRET_KEY,
+          },
+        },
+      },
+    },
+  ],
 });
